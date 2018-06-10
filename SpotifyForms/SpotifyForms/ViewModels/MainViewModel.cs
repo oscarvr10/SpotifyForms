89967d15c1a8535953a50ae43fcda003a5f674f0
Oscar Vera <vera.reyes.92@outlook.com>
2018-06-10 01:28:09 -0500

    Created initial views and viewmodel

---
+++ SpotifyForms/SpotifyForms/ViewModels/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using SpotifyForms.Core.Models;
+using SpotifyForms.Core.ViewModels.Base;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SpotifyForms.Core.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        #region Properties
+
+        public List<Album> Albums { get; set; }
+        public List<Playlist> Playlists { get; set; }
+        public List<Song> Songs { get; set; }
+
+        #endregion
+
+        #region Commands
+        
+
+        #endregion
+        public MainViewModel()
+        {
+
+        }
+
+
+        #region Base Methods
+
+        public override Task InitializeAsync(object navigationData)
+        {
+            return base.InitializeAsync(navigationData);
+        }
+
+        #endregion
+
+
+        #region Command Methods
+        
+        #endregion
+
+    }
+}
---
+++ SpotifyForms/SpotifyForms/Views/MainView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SpotifyForms.Core.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainView : TabbedPage
+	{
+		public MainView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
