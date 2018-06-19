f0bd71d93a895942e8335e0dc82372a7c32990b5
Oscar Vera <vera.reyes.92@outlook.com>
2018-06-18 23:03:57 -0500

    Created HorizontalScrollView control and renderers for each platform

---
+++ SpotifyForms/SpotifyForms.Android/Renderers/HorizontalScrollViewRenderer.cs
@@ -0,0 +1,42 @@
+﻿using Android.Content;
+using SpotifyForms.Core.Views.Controls;
+using SpotifyForms.Droid.Renderers;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(HorizontalScrollView), typeof(HorizontalScrollViewRenderer))]
+namespace SpotifyForms.Droid.Renderers
+{
+    public class HorizontalScrollViewRenderer : ScrollViewRenderer
+    {
+        public HorizontalScrollViewRenderer(Context context): base(context)
+        {
+        }
+
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            var element = e.NewElement as HorizontalScrollView;
+            element?.Render();
+
+            if (e.OldElement != null || this.Element == null)
+                return;
+
+            if (e.OldElement != null)
+                e.OldElement.PropertyChanged -= OnElementPropertyChanged;
+
+            e.NewElement.PropertyChanged += OnElementPropertyChanged;
+        }
+
+        protected void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (ChildCount > 0)
+            {
+                GetChildAt(0).HorizontalScrollBarEnabled = false;
+                GetChildAt(0).VerticalScrollBarEnabled = false;
+            }
+        }
+    }
+}
---
+++ SpotifyForms/SpotifyForms.iOS/Renderers/HorizontalScrollViewRenderer.cs
@@ -0,0 +1,22 @@
+﻿using SpotifyForms.Core.Views.Controls;
+using SpotifyForms.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(HorizontalScrollView), typeof(HorizontalScrollViewRenderer))]
+namespace SpotifyForms.iOS.Renderers
+{
+    public class HorizontalScrollViewRenderer : ScrollViewRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            var element = e.NewElement as HorizontalScrollView;
+            element?.Render();
+
+            // Don't need these.
+            ShowsHorizontalScrollIndicator = false;
+        }
+    }
+}
