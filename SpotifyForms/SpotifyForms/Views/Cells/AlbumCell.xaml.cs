84f0c7805567859b249668c0a93084206bf87181
Oscar Vera <vera.reyes.92@outlook.com>
2018-06-21 00:21:28 -0500

    Added cell design for album, artist and podcast lists

---
+++ SpotifyForms/SpotifyForms/Views/Cells/AlbumCell.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SpotifyForms.Core.Views.Cells
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AlbumCell : ViewCell
+	{
+		public AlbumCell ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ SpotifyForms/SpotifyForms/Views/Cells/PodcastCell.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SpotifyForms.Core.Views.Cells
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PodcastCell : ViewCell
+	{
+		public PodcastCell ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
