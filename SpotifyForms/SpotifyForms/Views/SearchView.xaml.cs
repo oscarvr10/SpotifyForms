c512eae053ec3466b333edc4accb15eed5dd79f0
Oscar Vera <vera_reyes-10@hotmail.com>
2018-07-29 00:28:24 -0500

    Finished SearchView

--- SpotifyForms/SpotifyForms/Views/SearchView.xaml.cs
+++ SpotifyForms/SpotifyForms/Views/SearchView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace SpotifyForms.Core.Views
@@ -10,5 +11,26 @@ namespace SpotifyForms.Core.Views
 		{
 			InitializeComponent();
 		}
-	}
+
+        void Handle_Scrolled(object sender, Xamarin.Forms.ScrolledEventArgs e)
+        {
+            
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            //Because the ListView is within ScrollView
+            //We set the Height of StackLayout that contains the mentioned ListView, in order ScrollView can be scrolled
+            stkContainer.HeightRequest = GetGridContainerHeight((double)flvCategories.FlowItemsSource.Count, 
+                                                                (double)flvCategories.FlowColumnCount, 
+                                                                flvCategories.RowHeight) + 80;
+            
+        }
+        public static double GetGridContainerHeight(double itemCount, double columnCount, int rowHeight) 
+        { 
+            var rowCount = Math.Ceiling(itemCount / columnCount); 
+            return rowCount * rowHeight; 
+        }
+    }
 }
