3948229cebe0319747c8b44a9bbcbb4e6866339a
Oscar Vera <vera_reyes-10@hotmail.com>
2018-08-04 18:53:20 -0500

    some UI improvements

--- SpotifyForms/SpotifyForms/Views/HomeView.xaml.cs
+++ SpotifyForms/SpotifyForms/Views/HomeView.xaml.cs
@@ -15,8 +15,42 @@ namespace SpotifyForms.Core.Views
         {
             if (gradientView.Height <= 0)
                 return;
+            
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                if (e.ScrollY <= 90)
+                {
+                    if (!ToolbarItems.Contains(tbiSettings))
+                        ToolbarItems.Add(tbiSettings);
 
-            gradientView.TranslationY = -(int)((float)e.ScrollY / 3.5F);
+                    gradientView.FadeTo(1.0, 400);
+                }
+                else
+                {
+                    if (ToolbarItems.Contains(tbiSettings))
+                        ToolbarItems.Remove(tbiSettings);
+                    
+                    gradientView.FadeTo(-(int)((float)e.ScrollY / 5.5F), 400);
+                }
+            }
+            else
+            {
+                if (e.ScrollY <= 20)
+                {
+                    if (!ToolbarItems.Contains(tbiSettings))
+                        ToolbarItems.Add(tbiSettings);
+                    
+                    gradientView.FadeTo(1.0, 400);
+                }
+                else
+                {
+                    if (ToolbarItems.Contains(tbiSettings))
+                        ToolbarItems.Remove(tbiSettings);
+                    
+                    gradientView.FadeTo(-(int)((float)e.ScrollY / 5.5F), 400);
+                }
+            }
+
         }
     }
 }
--- SpotifyForms/SpotifyForms/Views/SearchView.xaml.cs
+++ SpotifyForms/SpotifyForms/Views/SearchView.xaml.cs
@@ -26,50 +26,68 @@ namespace SpotifyForms.Core.Views
 
         void mainScrollView_Scrolled(object sender, Xamarin.Forms.ScrolledEventArgs e)
         {
-            double headerHeight = 100;
-
-            //if (e.ScrollY <= 0)
-            //{
-            //    mainScrollView.TranslationY = 0;
-            //    if (gridContainer.Children.Contains(searchBar))
-            //    {
-            //        var index = gridContainer.Children.IndexOf(searchBar);
-            //        gridContainer.Children.RemoveAt(index);
-            //    }
-            //}
-            if (e.ScrollY <= (headerHeight * 2))
+            //Needs to be reworked because it doesn't work very well on both platforms
+            /*if (Device.RuntimePlatform == Device.Android)
             {
-                if (gridContainer.Children.Contains(searchBar))
+                double headerHeight = 100;
+
+                if (e.ScrollY <= (headerHeight * 2))
                 {
-                    var index = gridContainer.Children.IndexOf(searchBar);
-                    gridContainer.Children.RemoveAt(index);
+                    if (gridContainer.Children.Contains(searchBar))
+                    {
+                        var index = gridContainer.Children.IndexOf(searchBar);
+                        gridContainer.Children.RemoveAt(index);
+                    }
+
+                    mainScrollView.TranslationY = 0;
                 }
+                else if (e.ScrollY > (headerHeight * 2)) //scrolled 
+                {
+                    if (!gridContainer.Children.Contains(searchBar))
+                        gridContainer.Children.Add(searchBar);
 
-                mainScrollView.TranslationY = 0;
-            }
-            else if (e.ScrollY > (headerHeight * 2)) //scrolled 
+                    mainScrollView.TranslationY = headerHeight;
+                }
+            }*/
+            if(Device.RuntimePlatform == Device.Android)
             {
-                if (!gridContainer.Children.Contains(searchBar))
-                    gridContainer.Children.Add(searchBar);
-
-                mainScrollView.TranslationY = headerHeight;
+                if (e.ScrollY <= 25)
+                    gradientView.FadeTo(1.0, 400);
+                else
+                    gradientView.FadeTo(-(int)((float)e.ScrollY / 5.5F), 400);
             }
-
+            else
+            {
+                if (e.ScrollY <= 5)
+                    gradientView.FadeTo(1.0, 400);
+                else
+                    gradientView.FadeTo(-(int)((float)e.ScrollY / 5.5F), 400);
+            }
         }
 
         protected override void OnAppearing()
         {
             base.OnAppearing();
             //Because the ListView is within ScrollView
-            //We set the Height of StackLayout that contains the mentioned ListView, in order ScrollView can be scrolled
-            stkContainer.HeightRequest = GetGridContainerHeight((double)flvCategories.FlowItemsSource.Count, 
-                                                                (double)flvCategories.FlowColumnCount, 
-                                                                flvCategories.RowHeight) + 100;
+            //We manually set the StackLayout Height that contains the mentioned ListView, in order ScrollView can be scrolled until to the bottom
+
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                stkContainer.HeightRequest = GetGridContainerHeight((double)flvCategories.FlowItemsSource.Count,
+                                                                    (double)flvCategories.FlowColumnCount,
+                                                                    flvCategories.RowHeight + 40);
+            }
+            else
+            {
+                stkContainer.HeightRequest = GetGridContainerHeight((double)flvCategories.FlowItemsSource.Count,
+                                                                    (double)flvCategories.FlowColumnCount,
+                                                                    flvCategories.RowHeight + 20);
+            }
             
         }
         public static double GetGridContainerHeight(double itemCount, double columnCount, int rowHeight) 
         { 
-            var rowCount = Math.Ceiling(itemCount / columnCount); 
+            var rowCount = Math.Round(itemCount / columnCount); 
             return rowCount * rowHeight; 
         }
     }
